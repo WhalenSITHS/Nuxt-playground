88b9147ec89c568a8bd3108dee53f00dbd399b94
Michael Whalen <michael.whalen@sitechhs.com>
2020-05-31 14:54:28 -0400

    testing  hamburger

--- whalen-site/nuxt.config.js
+++ whalen-site/nuxt.config.js
@@ -23,12 +23,12 @@ export default {
         rel: "stylesheet",
         href:
           "https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400&display=swap"
-      },
-      {
+      }
+      /* {
         rel: "stylesheet",
         href:
           "https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css"
-      }
+      } */
     ]
   },
   /*
